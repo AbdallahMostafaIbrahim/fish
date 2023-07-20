aa9e153a4f25312ab55ebc22ac8bb0df9e19d798
AbdallahMostafaIbrahim <abdallahmostafaibrahim@gmail.com>
2023-07-21 00:52:02 +0300

    ms login page and console log creds

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import express from "express";
-import { basePath } from "../config.json";
+import config from "../config.json" assert { type: "json" };
 import { connect } from "mongoose";
-import apiRouter from "./api";
+import apiRouter from "./api.js";
+import cors from "cors";
+import path from "path";
 
 await connect("mongodb://127.0.0.1:27017/fish");
 
 const app = express();
 
-app.get(basePath, (req, res) => {
-  res.send("Hello");
-});
+app.use(cors());
+
+app.use(config.basePath, express.static(path.join(process.cwd(), "./public/")));
 
 app.use("/api", apiRouter);
 
-app.get("*", (req, res) => {
-  res.redirect(basePath);
-});
+// app.get("*", (req, res) => {
+//   res.redirect(config.basePath);
+// });
 
-app.listen(80, () => {
+app.listen(8000, () => {
   console.log("Server Listening on port 80");
 });
